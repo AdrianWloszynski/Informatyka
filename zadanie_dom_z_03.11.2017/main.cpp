e70c6c1cdf30c4d39562646cf9628c45633ed445
Adrian Włoszyński <32750198+AdrianWloszynski@users.noreply.github.com>
2017-11-05 08:25:27 +0100

    Add files via upload

---
+++ zadanie_dom_z_03.11.2017/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    int ilosc_liczb,p,q,liczba1,ilosc_wierszy,i,j;
+    cout<<"Podaj ilosc liczb: ";
+    cin>>ilosc_liczb;
+    cout<<"Podaj przedzial, z ktorego maja byc wylosowane"<<endl;
+    cout<<"Poczatek przedzialu : ";
+    cin>>p;
+    cout<<"Koniec przedzialu : ";
+    cin>>q;
+    while(p>q)
+    {
+        cout<<""<<endl;
+        cout<<"Bledne dane"<<endl;
+        cout<<"Podaj przedzial, z ktorego maja byc wylosowane"<<endl;
+        cout<<"Poczatek przedzialu : ";
+        cin>>p;
+        cout<<"Koniec przedzialu : ";
+        cin>>q;
+    }
+    cout<<"Podaj ilosc wierszy: ";
+    cin>>ilosc_wierszy;
+    liczba1=p+rand()%(q-p+1);
+    for(i=1;i<=ilosc_wierszy;i++)
+        {
+            for(j=1;j<=ilosc_liczb;j++)
+                cout<<liczba1<<" ";
+            cout<<" "<<endl;
+        }
+    system("pause");
+
+    return 0;
+}
